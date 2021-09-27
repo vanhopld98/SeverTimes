415bb1734a42d58342da63e3eface424a6391de5
vanhopld98 <vanhopld98@gmail.com>
2021-09-27 10:41:03 +0700

    done

---
+++ src/main/java/SeverTimeServlet.java
@@ -0,0 +1,23 @@
+import javax.servlet.*;
+import javax.servlet.http.*;
+import javax.servlet.annotation.*;
+import java.io.IOException;
+import java.io.PrintWriter;
+import java.util.Date;
+
+@WebServlet(name = "SeverTimeServlet", value = "/index")
+public class SeverTimeServlet extends HttpServlet {
+    @Override
+    protected void doGet(HttpServletRequest request, HttpServletResponse response) throws ServletException, IOException {
+        PrintWriter printWriter = response.getWriter();
+        printWriter.println("<html>");
+        Date today = new Date();
+        printWriter.println("<h1>" + today + "</h1>");
+        printWriter.println("</html>");
+    }
+
+    @Override
+    protected void doPost(HttpServletRequest request, HttpServletResponse response) throws ServletException, IOException {
+
+    }
+}
